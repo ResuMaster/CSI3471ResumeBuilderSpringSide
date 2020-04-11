7a0c9e1d4bec8da18b76b1122d6d1eae0057fe42
nnazo <jacobcurtis2266@gmail.com>
2020-04-10 22:55:26 -0500

    add pdf endpoints

--- src/main/java/to/us/resume_builder/spring/controller/PDFController.java
+++ src/main/java/to/us/resume_builder/spring/controller/PDFController.java
@@ -1,15 +1,18 @@
 package to.us.resume_builder.spring.controller;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import to.us.resume_builder.spring.data.pdf.PdfDBCHandle;
 import to.us.resume_builder.spring.data.pdf.abstract_dbc.PdfDBC;
@@ -66,5 +69,111 @@ public class PDFController extends BasicController {
 			return getOkResponse().build();
 		else
 			return ResponseEntity.badRequest().build();
+	}
+
+	@PostMapping(name = "/pdf/url", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<InputStreamResource> postPdfUrl(@RequestParam(name = "latex") String latex) {
+		Path pdf;
+		InputStreamResource rsc;
+		try {
+			pdf = export(latex);
+			String response = uploadPDF(pdf);
+			rsc = new InputStreamResource(new ByteArrayInputStream(response.getBytes()));
+		} catch (IOException | InterruptedException e) {
+			return ResponseEntity.status(500).build();
+		}
+		return getOkResponse().contentType(MediaType.APPLICATION_JSON).body(rsc);
+	}
+
+	@PostMapping(name = "/pdf", produces = MediaType.APPLICATION_PDF_VALUE)
+	public ResponseEntity<InputStreamResource> postPdf(@RequestParam(name = "latex") String latex) {
+		Path pdf;
+		InputStreamResource rsc;
+		try {
+			pdf = export(latex);
+			rsc = new InputStreamResource(new BufferedInputStream(new FileInputStream(String.valueOf(pdf))));
+		} catch (IOException e) {
+			return ResponseEntity.status(500).build();
+		}
+		return getOkResponse().contentType(MediaType.APPLICATION_PDF).body(rsc);
+	}
+
+	private String uploadPDF(Path pdf) throws IOException, InterruptedException {
+		ProcessBuilder builder = new ProcessBuilder("curl", "-F", "\"file=@" + pdf.toString() + "\"", "https://file.io");
+		builder.directory(pdf.getParent().toFile());
+		builder.redirectOutput(new File("./fileio.log"));
+		builder.redirectError(new File("./fileio_error.log"));
+		Process p = builder.start();
+		/*ApplicationConfiguration.getInstance().getLong("export.timeout")*/
+		if (!p.waitFor(60L, TimeUnit.SECONDS)) {
+			p.destroy();
+		}
+		return Files.readString(pdf);
+	}
+
+	private Path export(String latex) throws IOException {
+		Path latexPath = Path.of("./", randomAlphanumericString(16) + ".tex");
+		Path exportLocation = Path.of("./", "resume.pdf");
+		if (!Files.exists(latexPath.getParent())) {
+			// Generate the temp folder
+			Files.createDirectory(latexPath.getParent());
+			// Generate the LaTeX file
+			Files.writeString(latexPath, latex, StandardCharsets.UTF_8, StandardOpenOption.CREATE);
+			// Generate the PDF
+			boolean status = compileResumePDF(latexPath);
+			// Save the pdf to the specified location
+			if (status) {
+				Files.move(latexPath.resolveSibling(latexPath.getFileName().toString().split("\\.")[0] + ".pdf"), exportLocation, StandardCopyOption.REPLACE_EXISTING);
+			} else {
+				Files.deleteIfExists(latexPath.resolveSibling(latexPath.getFileName().toString().split("\\.")[0] + ".pdf"));
+			}
+		}
+		return exportLocation;
+	}
+
+	private boolean compileResumePDF(Path filePath) throws IOException {
+		// Temporary artifacts
+		final String[] ARTIFACTS_TO_DELETE = { "aux", "log", "tex" };
+		boolean status = true;
+
+		// Attempt to generate the resume
+		try {
+			ProcessBuilder builder = new ProcessBuilder("pdflatex", "\"" + filePath.toAbsolutePath().toString() + "\"");
+			builder.directory(filePath.getParent().toFile());
+			// TODO: add dedicated log file
+			builder.redirectOutput(new File("./export.log"));
+			builder.redirectError(new File("./export.log"));
+
+			// Run the process
+			Process p = builder.start();
+			/*ApplicationConfiguration.getInstance().getLong("export.timeout")*/
+			if (!p.waitFor(60L, TimeUnit.SECONDS)) {
+				p.destroy();
+				status = false;
+			}
+
+			// Clean up artifacts
+			for (File f : Objects.requireNonNull(filePath.getParent().toFile().listFiles())) {
+				if (f.isFile() && Arrays.stream(ARTIFACTS_TO_DELETE).anyMatch(e -> f.getName().endsWith(e))) {
+					Files.deleteIfExists(f.toPath());
+				}
+			}
+		} catch (InterruptedException e) {
+			status = false;
+		}
+
+		return status;
+	}
+
+	private static String randomAlphanumericString(int length) {
+		int leftLimit = 48;  // Numeral '0'
+		int rightLimit = 122; // Letter 'z'
+		Random random = new Random();
+
+		return random.ints(leftLimit, rightLimit + 1)
+			.filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))  // filter out non-alphanumerics
+			.limit(length)
+			.collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+			.toString();
 	}
 }
