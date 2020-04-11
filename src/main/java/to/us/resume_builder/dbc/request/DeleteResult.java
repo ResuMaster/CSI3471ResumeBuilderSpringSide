fa51d4b7bc289c539fcbd047fa2df2bf38484fa9
Micah <micah_schiewe1@baylor.edu>
2020-04-10 19:13:18 -0500

    Copy client from other project

---
+++ src/main/java/to/us/resume_builder/dbc/request/DeleteResult.java
@@ -0,0 +1,5 @@
+package to.us.resume_builder.dbc.request;
+
+public enum DeleteResult {
+	SUCCESS, FAILURE, DOES_NOT_EXIST
+}
---
+++ src/main/java/to/us/resume_builder/dbc/request/RequestType.java
@@ -0,0 +1,5 @@
+package to.us.resume_builder.dbc.request;
+
+public enum RequestType {
+	GET, SET, PUT, DELETE
+}
