f010c4f9ea4c5b204dfe494e4986c11fe99a9bb4
nnazo <jacobcurtis2266@gmail.com>
2020-04-15 22:04:16 -0500

    refactor and clean up resources

--- src/main/java/to/us/resume_builder/spring/controller/BasicController.java
+++ src/main/java/to/us/resume_builder/spring/controller/BasicController.java
@@ -8,7 +8,8 @@ import org.springframework.http.ResponseEntity.BodyBuilder;
 /**
  * Provides basic methods needed by a other Controllers.
  * 
- * @author Micah
+ * @author Micah Schiewe
+ * @author Jacob Curtis
  */
 public abstract class BasicController {
 	/**
@@ -16,11 +17,58 @@ public abstract class BasicController {
 	 * 
 	 * @return A builder, ready to build a standard OK response.
 	 */
-	protected BodyBuilder getOkResponse() {
+	protected BodyBuilder ok() {
 		return ResponseEntity.ok().headers(new HttpHeaders());
 	}
 
-	protected BodyBuilder getInternalServerErrorResponse(String errorMessage) {
-		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).headers(new HttpHeaders()).header("error", errorMessage);
+	/**
+	 * Creates a basic http error response builder with a specified message.
+	 *
+	 * @param status The error status
+	 * @param errorMessage The error message to be specified in the header
+	 * @return The builder, ready to build the response.
+	 */
+	protected BodyBuilder error(HttpStatus status, String errorMessage) {
+		return ResponseEntity.status(status).headers(new HttpHeaders()).header("error", errorMessage);
+	}
+
+	/**
+	 * Creates an internal server error response.
+	 *
+	 * @param errorMessage The error message to be specified in the header
+	 * @return The builder, ready to build the response.
+	 */
+	protected BodyBuilder internalServerError(String errorMessage) {
+		return error(HttpStatus.INTERNAL_SERVER_ERROR, errorMessage);
+	}
+
+	/**
+	 * Creates a request timeout error response.
+	 *
+	 * @param errorMessage The error message to be specified in the header
+	 * @return The builder, ready to build the response.
+	 */
+	protected BodyBuilder requestTimeout(String errorMessage) {
+		return error(HttpStatus.REQUEST_TIMEOUT, errorMessage);
+	}
+
+	/**
+	 * Creates a bad request error response.
+	 *
+	 * @param errorMessage The error message to be specified in the header
+	 * @return The builder, ready to build the response.
+	 */
+	protected BodyBuilder badRequest(String errorMessage) {
+		return error(HttpStatus.BAD_REQUEST, errorMessage);
+	}
+
+	/**
+	 * Creates a service unavailable error response.
+	 *
+	 * @param errorMessage The error message to be specified in the header
+	 * @return The builder, ready to build the response.
+	 */
+	protected BodyBuilder serviceUnavailable(String errorMessage) {
+		return error(HttpStatus.SERVICE_UNAVAILABLE, errorMessage);
 	}
 }
