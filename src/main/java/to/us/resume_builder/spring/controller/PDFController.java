5e497784a1a21ec71e224dc79ac6dfdc6eb42bfd
Micah <micah_schiewe1@baylor.edu>
2020-04-19 21:50:35 -0500

    Add tests for endpoint
    * You will need to add the following as a VM argument to run the tests:
        -javaagent:path/to/mockit/mockit-version.jar

--- src/main/java/to/us/resume_builder/spring/controller/PDFController.java
+++ src/main/java/to/us/resume_builder/spring/controller/PDFController.java
@@ -1,23 +1,18 @@
 package to.us.resume_builder.spring.controller;
 
-import java.io.*;
-import java.nio.file.*;
-import java.util.concurrent.TimeoutException;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.springframework.core.io.InputStreamResource;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import to.us.resume_builder.MiscUtils;
-import to.us.resume_builder.ResumeExporter;
+import to.us.resume_builder.spring.service.PDFService;
 
 /**
- * PDFController contains the /pdf endpoint allowing a caller to give a
- * LaTeX string and compile it to PDF and have it returned directly or via
- * file.io
+ * PDFController contains the /pdf endpoint allowing a caller to give a LaTeX
+ * string and compile it to PDF and have it returned directly or via file.io
  *
  * @author Jacob Curtis
  * @author Micah Schiewe
@@ -26,66 +21,24 @@ import to.us.resume_builder.ResumeExporter;
 public class PDFController extends BasicController {
 	private static final Logger LOG = Logger.getLogger(PDFController.class.getName());
 
+	private PDFService service;
+
+	public PDFController() {
+		service = new PDFService();
+	}
+
 	/**
-	 * This endpoint takes a latex string and a url boolean specifying if the
-	 * caller wants the compiled PDF file to be uploaded to file.io instead of
-	 * being directly returned in the response.
+	 * This endpoint takes a latex string and a url boolean specifying if the caller
+	 * wants the compiled PDF file to be uploaded to file.io instead of being
+	 * directly returned in the response.
 	 *
 	 * @param latex The LaTeX string to be compiled to PDF.
-	 * @param url Whether the caller wants the PDF uploaded to file.io
+	 * @param url   Whether the caller wants the PDF uploaded to file.io
 	 * @return The response the user requested, or possibly an error.
 	 */
 	@PostMapping("/pdf")
 	public ResponseEntity<InputStreamResource> postPdf(@RequestParam(name = "latex") String latex,
-													   @RequestParam(name = "url") boolean url) {
-		Path pdf;
-		do {
-			pdf = Path.of("./", MiscUtils.randomAlphanumericString(16) + ".pdf");
-		} while (Files.exists(pdf));
-
-		InputStreamResource rsc;
-		MediaType mediaType;
-		InputStream in;
-		try {
-			if (!ResumeExporter.export(pdf, latex)) {
-				LOG.warning("PDF compilation failed");
-				return badRequest("Could not convert LaTeX to PDF").build();
-			}
-
-			LOG.info("requesting url: " + url);
-			if (url) {
-				// Upload PDF to file.io, return response
-				LOG.info("Uploading pdf to file.io");
-				String response = ResumeExporter.uploadPDF(pdf);
-				in = new ByteArrayInputStream(response.getBytes());
-				mediaType = MediaType.APPLICATION_JSON;
-			} else {
-				// Give the PDF in the response
-				in = new ByteArrayInputStream(Files.readAllBytes(pdf));
-				mediaType = MediaType.APPLICATION_PDF;
-			}
-		} catch (FileNotFoundException e) {
-			LOG.warning("could not find " + pdf.toString() + " - " + e.getMessage());
-			return internalServerError("internal file not found").build();
-		} catch (InterruptedException e) {
-			LOG.warning("file.io upload failed - " + e.getMessage());
-			return serviceUnavailable("file.io upload failed").build();
-		} catch (TimeoutException e) {
-			LOG.warning("file.io upload timed out - " + e.getMessage());
-			return requestTimeout("file.io upload timed out").build();
-		} catch (IOException e) {
-			LOG.logp(Level.WARNING, getClass().getName(), "postPdf", "IOException", e);
-			return internalServerError("internal file I/O error").build();
-		}
-
-		// Clean up PDF file
-		try {
-			Files.deleteIfExists(pdf.toAbsolutePath());
-		} catch (IOException e) {
-			LOG.warning("could not delete " + pdf.toString() + " - " + e);
-		}
-
-		rsc = new InputStreamResource(new BufferedInputStream(in));
-		return ok().contentType(mediaType).body(rsc);
+			@RequestParam(name = "url") boolean url) {
+		return service.handleRequest(latex, url);
 	}
 }
